ba2ff6eeb4d0870d79652d50c86b72e5a48b0c30
glitchmaker <glitchmakers@gmail.com>
2014-08-21 21:26:12 +0100

    Corrected Algorithm
    Precision corrections. changed from floats to double.

--- gregtojd.c
+++ gregtojd.c
@@ -8,34 +8,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
-
-float convert(int year, int month, int day){
-    int centuries=0;
+double convert(int y, int m, double d){
+    int a=0;
     int b =0;
     int c =0;
     int e =0;
     int f =0;
-    float jd =0;
+    double jd =0;
     
-    centuries = year /100;
-    b = centuries/4;
-    c = 2 - centuries - b;
-    e = 365.25 * (year+4716);
-    f = 30.6001 * (month-1);
-    jd =c + day + e + f - 1524.5;
+    a = y /100;
+    b = a/4;
+    c = 2 - a + b;
+    e = (int)(365.25 * (y+4716));
+    f = (int)(30.6001 * (m+1));
+    jd =c + d + e + f - 1524.5;
     return jd;
 }
 int main(int argc, char** argv) {
     int year=0;
     int month =0;
-    int day = 0;
+    float day = 0.0;
     printf ("Date in format YYYY/M/D:");
-    scanf ("%d/%d/%d",&year,&month,&day);
-    if (month==1 || month==2){
+    scanf ("%d/%d/%f",&year,&month,&day);
+    if (month <= 2){
         year-=1;
+        month+=12;
     }
     printf("%f\n",convert(year,month,day));
     return (EXIT_SUCCESS);
